e79ef853463ceb6231adc15ec8f8f8fb35e07d20
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-30 14:54:45 -0400

    Whatsapp Clone Creating Sidebar Component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Avatar, IconButton } from "@material-ui/core";
+import { Avatar, Button, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
@@ -20,7 +20,10 @@ function Sidebar() {
       </Header>
       <Search>
         <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
       </Search>
+
+      <SidebarButton>Start a new chat</SidebarButton>
     </Container>
   );
 }
@@ -33,7 +36,7 @@ const Header = styled.div`
   display: flex;
   position: sticky;
   top: 0;
-  background-color: #ffeeed;
+  background-color: #b76e79;
   z-index: 1;
   justify-content: space-between;
   align-items: center;
@@ -50,6 +53,23 @@ const UserAvatar = styled(Avatar)`
   }
 `;
 
-const Search = styled.div``;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
 
 const IconContainer = styled.div``;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+  border-top: 1px solid whitesmoke;
+  border-bottom: 1px solid whitesmoke;
+`;
